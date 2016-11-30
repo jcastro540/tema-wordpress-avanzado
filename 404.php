e245ebc9a947a557ceb9fb95c488076c455bd9e5
José Castro <josecastro540gmail.com>
2016-11-30 18:59:32 -0400

    clase 10

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<section class="error-404">
+	<div class="mensajes">
+		<h1>404</h1>
+		<h2>Página no encontrada</h2>
+	</div>
+	
+</section>
+
+
+<?php get_footer(); ?>
--- footer.php
+++ footer.php
@@ -1,4 +1,50 @@
-
 	<?php wp_footer(); ?>
+	<footer>
+		<section class="container">
+			<hr>
+			<div class="row">
+				<article class="col-md-6">
+					<?php 
+						$menuFooter = array(
+							'theme_location' => 'footer-menu',
+							'menu_id' => 'menu-footer',
+							'menu_class'=> 'list-inline',
+							'depth' => 2 
+						);
+					 ?>
+					<?php wp_nav_menu($menuFooter); ?>
+				</article>
+				<article class="col-md-6">
+					<ul class="list-inline text-right">
+						<li>
+							<a href="">
+								<span class="fa-stack fa-lg">
+								  <i class="fa fa-square-o fa-stack-2x"></i>
+								  <i class="fa <?php echo get_theme_mod('nosotros_icono1');?> fa-stack-1x"></i>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<span class="fa-stack fa-lg">
+								  <i class="fa fa-square-o fa-stack-2x"></i>
+								  <i class="fa fa-twitter fa-stack-1x"></i>
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<span class="fa-stack fa-lg">
+								  <i class="fa fa-square-o fa-stack-2x"></i>
+								  <i class="fa fa-instagram fa-stack-1x"></i>
+								</span>
+							</a>
+						</li>
+					</ul>
+				</article>
+			</div>
+		</section>
+	</footer>
+
 </body>
 </html>
